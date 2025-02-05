d32576a466594f666731dc7ded4b2566e121dfb6
Ssewannonda Keith Edwin <keith@laboremus.ug>
2025-02-05 11:29:17 +0300

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/investment-calculator-logo.png'
+
+function Header() {
+  return (
+    <header>
+        <img src={logo} alt="logo having a money bag" />
+        <h1>Investment Calculator</h1>
+    </header>
+  )
+}
+
+export default Header
